1381f03ec695f26da364b34234f7d9b8d2562b47
dnjs0 <heatherray@naver.com>
2025-04-10 11:25:45 +0900

    controller

---
+++ WebTest/src/main/java/com/test/web/controller/Ex040Controller.java
@@ -0,0 +1,19 @@
+package com.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="/ex04.do")
+public class Ex040Controller {
+	
+	@RequestMapping
+	public String call() {
+		
+		
+		
+		return "ex04";
+	}
+	
+	
+}
---
+++ WebTest/src/main/java/com/test/web/controller/Ex042Controller.java
@@ -0,0 +1,16 @@
+package com.test.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/ex04_2.do", method=RequestMethod.POST)
+public class Ex042Controller {
+	
+	@RequestMapping
+	public String call() {
+		return "ex042";
+	}
+
+}
