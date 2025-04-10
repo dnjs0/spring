c033014c515bab47628a6d73aca06e2cdf17cf4b
dnjs0 <heatherray@naver.com>
2025-04-10 17:46:55 +0900

    JUnit

---
+++ JunitTest/src/main/java/com/test/java/service/TestService.java
@@ -0,0 +1,15 @@
+package com.test.java.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TestService {
+
+	public int work(String word) {
+		
+		return word.length();
+		
+	}
+	
+	
+}
