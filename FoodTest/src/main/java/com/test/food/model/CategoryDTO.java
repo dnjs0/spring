6c4a906f47adc0baa6c9b53e43dce9f88835d3e5
dnjs0 <heatherray@naver.com>
2025-04-17 16:24:31 +0900

    디비작업

---
+++ FoodTest/src/main/java/com/test/food/model/CategoryDTO.java
@@ -0,0 +1,15 @@
+package com.test.food.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+public class CategoryDTO {
+	private String seq;
+	private String name;
+	private String marker;
+}
